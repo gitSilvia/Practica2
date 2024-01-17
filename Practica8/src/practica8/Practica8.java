55c3e1cdb1381177b0a445631a55beed82f9b87c
gitSilvia <154083502+gitSilvia@users.noreply.github.com>
2024-01-17 14:15:18 -0300

    Add files via upload

---
+++ Practica8/src/practica8/Practica8.java
@@ -0,0 +1,17 @@
+
+package practica8;
+
+/**
+ *
+ * @author silvia chamorro
+ */
+public class Practica8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
