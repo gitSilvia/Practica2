e1410eef4321bda8047ede74730583c40f45e009
gitSilvia <154083502+gitSilvia@users.noreply.github.com>
2024-01-17 14:14:29 -0300

    Add files via upload

---
+++ Practica7/src/practica7/Practica7.java
@@ -0,0 +1,17 @@
+
+package practica7;
+
+/**
+ *
+ * @author silvia chamorro
+ */
+public class Practica7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
